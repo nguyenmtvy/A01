9338e090064e91e2b41fb3b43efbc9d471e45953
nguyenmtvy <44111934+nguyenmtvy@users.noreply.github.com>
2019-05-29 21:26:06 +0700

    Final change

---
+++ Assets/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : MonoBehaviour
+{
+    public Vector2 newPos;
+    //":" means inherritance
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(newPos);
+        //GetComponent<Transform>().position = newPos;
+    }
+}
